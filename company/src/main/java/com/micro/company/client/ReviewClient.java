217a60671da0da47169e2adda0959704a56d98ef
eeshunarayan15 <eeshunarayan15@gmail.com>
2025-11-01 23:15:19 +0000

    updated company review client

--- company/src/main/java/com/micro/company/client/ReviewClient.java
+++ company/src/main/java/com/micro/company/client/ReviewClient.java
@@ -5,8 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "REVIEWS",
-        url="${review-service.url}")
+@FeignClient(name = "REVIEWS")
 public interface ReviewClient {
     @GetMapping("/api/v1/reviews/averageRating")
     public Apiresponse<Double> getAverageRating(@RequestParam Long companyId) ;
