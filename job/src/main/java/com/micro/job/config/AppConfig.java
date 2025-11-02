66913db00fb8c58a51daefdea401c1e31d7f48ab
eeshunarayan15 <eeshunarayan15@gmail.com>
2025-11-02 21:29:34 +0000

    updating job service

--- job/src/main/java/com/micro/job/config/AppConfig.java
+++ job/src/main/java/com/micro/job/config/AppConfig.java
@@ -3,14 +3,21 @@ package com.micro.job.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
+
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);  // 3 seconds to establish connection
+        factory.setReadTimeout(5000);     // 5 seconds to read response
 
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(factory);
+        return restTemplate;
     }
 }
