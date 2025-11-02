b1f614ad0d6b5945c70ac6195d3b5417d630a4c1
eeshunarayan15 <eeshunarayan15@gmail.com>
2025-11-02 10:37:06 +0000

    removed servelet context path

--- reviews/src/main/java/com/micro/reviews/clients/CompanyClients.java
+++ reviews/src/main/java/com/micro/reviews/clients/CompanyClients.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "COMPANY")
 public interface CompanyClients {
-    @GetMapping("/api/v1/public/{id}")
+    @GetMapping("/public/{id}")
     Apiresponse<CompanyDto> getCompanyById(@PathVariable(value = "id") Long id) ;
 }
